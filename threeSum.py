40b42944e3bfc10b403701578522f2d17c37302a
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-12 18:10:10 -0700

    Update threeSum.py

--- threeSum.py
+++ threeSum.py
@@ -1,12 +1,13 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums):
         out = []
-        nums.sort()
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
                     if nums[i] + nums[j] + nums[k] == 0:
                         out_temp = [nums[i], nums[j], nums[k]]
+                        out_temp.sort()
                         if out_temp not in out:
                             out.append(out_temp)
-        return set(out)
+        return out

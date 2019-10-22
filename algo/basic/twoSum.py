defdd37906c7183dadae10d255156c209a85bc9b
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-21 18:00:25 -0700

    Update twoSum.py

--- algo/basic/twoSum.py
+++ algo/basic/twoSum.py
@@ -1,4 +1,13 @@
 class Solution:
+    def twoSum_h(self, nums, target):
+        d = {}
+        for i in range(len(nums)):
+            if target - nums[i] not in d:
+                d[nums[i]] = i
+            else:
+                return [d[target - nums[i]],i]
+            
+            
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):

d2e49974adc9c38f2730b1de1a862bd7c4877026
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-21 21:58:09 -0700

    Create group-anagrams.py

---
+++ algo/string/group-anagrams.py
@@ -0,0 +1,31 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for w in sorted(strs):
+            key = tuple(sorted(w))
+            d[key] = d.get(key, []) + [w]
+        return d.values()
+        
+'''
+49. Group Anagrams
+Medium
+
+2139
+
+134
+
+Favorite
+
+Share
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+'''

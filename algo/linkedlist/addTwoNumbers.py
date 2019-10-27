9093a686b08a8c78fe7382f1df593985fa63a69e
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-27 15:08:22 -0700

    Update addTwoNumbers.py

--- algo/linkedlist/addTwoNumbers.py
+++ algo/linkedlist/addTwoNumbers.py
@@ -3,7 +3,22 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = curr = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, v = divmod(v1 + v2 + carry, 10)
+            curr.next = ListNode(v)
+            curr = curr.next
+        return head.next
+"""
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
@@ -23,3 +38,4 @@ class Solution:
         if carry > 0:
             curr.next = ListNode(carry)
         return head.next
+"""

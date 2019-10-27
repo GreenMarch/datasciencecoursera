cd0ce57432b39cf66b5b3dd800f88e118d0b3871
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-27 16:14:00 -0700

    Create next-greater-element-i.py

---
+++ algo/stack/next-greater-element-i.py
@@ -0,0 +1,62 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        dic = {}
+        for num in nums2:
+            while stack != [] and num > stack[-1]:
+                dic[stack.pop()] = num
+            stack.append(num)
+
+        res = []
+        for num in nums1:
+            if num in dic.keys():
+                res.append(dic[num])
+            else:
+                res.append(-1)
+
+        return res    
+    
+"""
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        out = [-1]*len(nums1)
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums2[j] == nums1[i]:
+                    for l in range(j + 1, len(nums2)):
+                        if nums2[l] > nums2[j]:
+                            out[i] = nums2[l]
+                            break  
+        return out
+"""
+"""
+496. Next Greater Element I
+Easy
+
+965
+
+1554
+
+Favorite
+
+Share
+You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+Output: [-1,3,-1]
+Explanation:
+    For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+    For number 1 in the first array, the next greater number for it in the second array is 3.
+    For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+Example 2:
+Input: nums1 = [2,4], nums2 = [1,2,3,4].
+Output: [3,-1]
+Explanation:
+    For number 2 in the first array, the next greater number for it in the second array is 3.
+    For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+Note:
+All elements in nums1 and nums2 are unique.
+The length of both nums1 and nums2 would not exceed 1000.
+"""

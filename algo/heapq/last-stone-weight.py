080522b3596c086cb0ab8dd1753508e7b0cb2958
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-27 18:25:41 -0700

    Create last-stone-weight.py

---
+++ algo/heapq/last-stone-weight.py
@@ -0,0 +1,60 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            print("len is one")
+            print(str(stones))
+            return stones[0]
+        else:
+            for idx, val in enumerate(stones):
+                stones[idx] = val*-1
+            heapq.heapify(stones)
+            res = 0
+            while len(stones) > 1:
+                stone1 = heapq.heappop(stones)
+                stone2 = heapq.heappop(stones)
+                if stone1 == stone2:
+                    continue
+                else:
+                    res = stone1 - stone2
+                    heapq.heappush(stones, res)
+            if stones:
+                return stones[0]*-1
+            else:
+                return 0
+"""
+1046. Last Stone Weight
+Easy
+
+234
+
+13
+
+Favorite
+
+Share
+We have a collection of rocks, each rock has a positive integer weight.
+
+Each turn, we choose the two heaviest rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+ 
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+ 
+
+Note:
+
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+"""

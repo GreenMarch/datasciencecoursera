f6adf102204180b5721b54c1842b019e76a2c807
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-29 17:38:57 -0700

    Create single-number.py

---
+++ algo/counter/single-number.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_table = {}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i] = 1
+        return hash_table.popitem()[0]
+    """
+        from collections import Counter
+        c = Counter(nums)
+        return c.most_common()[-1][0]"""
+        
+"""
+136. Single Number
+Easy
+
+2969
+
+115
+
+Favorite
+
+Share
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+"""

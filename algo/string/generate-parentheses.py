72c151e8b131231505cc58039bd0e0c3e7c0a6d0
GreenMarch <GreenMarch@users.noreply.github.com>
2019-10-30 23:57:17 -0700

    Update generate-parentheses.py

--- algo/string/generate-parentheses.py
+++ algo/string/generate-parentheses.py
@@ -52,7 +52,23 @@ class Solution(object):
 
         backtrack()
         return ans
-    
+        def generateParenthesis3(self, n):
+        if not n:
+            return []
+        left, right, ans = n, n, []
+        self.dfs(left,right, ans, "")
+        return ans
+
+    def dfs(self, left, right, ans, string):
+        if right < left:
+            return
+        if not left and not right:
+            ans.append(string)
+            return
+        if left:
+            self.dfs(left-1, right, ans, string + "(")
+        if right:
+            self.dfs(left, right-1, ans, string + ")")
 print(Solution().generateParenthesis2(3))
 
 """

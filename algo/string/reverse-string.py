ff95aa141e025421acaba44867c297248c0c480e
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-03 12:56:13 -0800

    Update reverse-string.py

--- algo/string/reverse-string.py
+++ algo/string/reverse-string.py
@@ -8,3 +8,76 @@ class Solution:
             s[i] = s[len(s) - 1 - i]
             s[len(s) - 1 - i] = temp
             
+ class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # return s.reverse()
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
+        
+class Solution(object):
+    def reverseStr_II(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        a = list(s)
+        for i in range(0, len(a), 2*k):
+            a[i:i+k] = reversed(a[i:i+k])
+        return "".join(a)
+"""
+541. Reverse String II
+Easy
+
+294
+
+914
+
+Favorite
+
+Share
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+"""
+
+"""
+344. Reverse String
+Easy
+
+921
+
+584
+
+Favorite
+
+Share
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+"""

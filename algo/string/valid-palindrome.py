3702464814436343c967d6aafc571f049bd4ef6a
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-03 22:17:41 -0800

    Update valid-palindrome.py

--- algo/string/valid-palindrome.py
+++ algo/string/valid-palindrome.py
@@ -1,4 +1,22 @@
 class Solution:
+    # correct
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+    # not pass
     def isPalindrome(self, s: str) -> bool:
         if len(s) <= 1:
             return True

c7aade4f3fb3f36ad1e5bbf3104fbf4eaceb0d4d
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-03 21:37:39 -0800

    Create remove-outermost-parentheses.py

---
+++ algo/stack/remove-outermost-parentheses.py
@@ -0,0 +1,67 @@
+class Solution(object):
+    def removeOuterParentheses(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        idx_out = {0}
+        L , R = 0, 0
+        for i in range(len(S)):
+            if S[i] == "(":
+                L += 1
+            else:
+                R += 1
+            if L == R:
+                idx_out.add(i)
+                idx_out.add(i+1)
+        return "".join([ch for idx, ch in enumerate(S) if idx not in idx_out])
+"""
+1021. Remove Outermost Parentheses
+Easy
+
+242
+
+366
+
+Favorite
+
+Share
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+"""

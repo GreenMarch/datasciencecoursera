d12635eb3c62e0ed290e40e6389dd80238a77b07
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-03 23:36:34 -0800

    Create letter-combinations-of-a-phone-number.py

---
+++ algo/dfs/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+
+        if not digits:
+            return []
+        dic = {"2":"abc", 
+               "3":"def", 
+               "4":"ghi", 
+               "5":"jkl", 
+               "6":"mno", 
+               "7":"pqrs", 
+               "8":"tuv", 
+               "9":"wxyz"}
+        res = []
+        self.dfs(digits, dic, 0, "", res)
+        return res
+
+    def dfs(self, digits, dic, index, path, res):
+        if len(path) == len(digits):
+            res.append(path)
+            return 
+        for i in range(index, len(digits)):
+            for j in dic[digits[i]]:
+                self.dfs(digits, dic, i + 1, path + j, res)
+                
+"""
+17. Letter Combinations of a Phone Number
+Medium
+
+2753
+
+345
+
+Favorite
+
+Share
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+"""

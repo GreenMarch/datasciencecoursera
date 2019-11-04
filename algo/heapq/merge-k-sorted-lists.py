ad5961dc92f57bb0eb3830ef4286819f53185ac8
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-03 22:51:52 -0800

    Create merge-k-sorted-lists.py

---
+++ algo/heapq/merge-k-sorted-lists.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+
+        h = [(l.val, idx) for idx, l in enumerate(lists) if l]
+        heapq.heapify(h)
+        head = cur = ListNode(None)
+        while h:
+            val, idx = heapq.heappop(h)
+            cur.next = ListNode(val)
+            cur = cur.next
+            node = lists[idx] = lists[idx].next
+            if node:
+                heapq.heappush(h, (node.val, idx))
+        return head.next
+        
+"""
+23. Merge k Sorted Lists
+Hard
+
+3166
+
+208
+
+Favorite
+
+Share
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+"""

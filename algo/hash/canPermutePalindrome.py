10369cd5c1607fdfa962b77956745e40e6e6306e
GreenMarch <GreenMarch@users.noreply.github.com>
2019-11-04 08:39:37 -0800

    Update canPermutePalindrome.py

--- algo/hash/canPermutePalindrome.py
+++ algo/hash/canPermutePalindrome.py
@@ -10,3 +10,29 @@ class Solution:
             if count1 > 1:
                 return False
         return True
+"""
+266. Palindrome Permutation
+Easy
+
+244
+
+42
+
+Favorite
+
+Share
+Given a string, determine if a permutation of the string could form a palindrome.
+
+Example 1:
+
+Input: "code"
+Output: false
+Example 2:
+
+Input: "aab"
+Output: true
+Example 3:
+
+Input: "carerac"
+Output: true
+"""

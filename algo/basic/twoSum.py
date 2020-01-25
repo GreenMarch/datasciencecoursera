eb953a560e2b88d9b0725db0e2174b0dbc7149ac
GreenMarch <lucuiwang@gmail.com>
2020-01-24 18:39:53 -0800

    test

--- algo/basic/twoSum.py
+++ algo/basic/twoSum.py
@@ -13,3 +13,8 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return i, j
+
+
+nums = [2, 7, 11, 15]
+target = 9
+Solution().twoSum_h(nums, target)

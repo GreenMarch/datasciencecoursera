a214e549d4289eb804a07dea141c39b136af26f7
GreenMarch <lucuiwang@gmail.com>
2020-02-03 00:01:28 -0800

    reorder

--- algo/string/reorder-data-in-log-files.py
+++ algo/string/reorder-data-in-log-files.py
@@ -1,16 +1,61 @@
 class Solution:
-    # pass
-    def reorderLogFiles2(self, logs):
-        """
-        :type logs: List[str]
-        :rtype: List[str]
-        """
-
+    def reorderLogFiles(self, logs):
         def f(log):
-            id_, rest = log.split(" ", 1)
-            return (0, rest, id_) if rest[0].isalpha() else (1,)
+            id, rest = log.split(" ", 1)
+            print("id ", str(id))
+            print("rest ", str(rest))
+            return (0, rest, id) if rest[0].isalpha() else (1,)
+        return sorted(logs, key=f)
 
-        return sorted(logs, key = f)
+    def reorderLogFiles2(self, logs):
+        letters = []
+        digits = []
+        for log in logs:
+            if log[-1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters = sorted(letters, key=lambda letter: (letter.split()[1:], letter.split()[0]))
+        return letters + digits
+
+    """
+"letter.split()[0]" means identifier
+"letter.split()[1:]" means content ignored identifier
+"key=lambda letter: (letter.split()[1:],letter.split()[0])" has two sorting keys. 
+    the first one "letter.split()[1:]" is priority 
+    and second one "letter.split()[0]" is backup
+Therefore, it means sort the letters array in lexicographical order and ignore identifier as priority, otherwise (for example two letters have same content)will keep the original order that is identifier order.
+    
+    
+student_tuples = [
+    ('A john', 'A', 15),
+    ('B jane', 'B', 12),
+    ('C dave', 'B', 10),
+]
+
+student_tuples = [
+    'A john',
+    'B mary',
+    'C dave',
+]
+
+sorted(student_tuples, key=lambda student: (student.split()[1], student.split()[0]))
+
+
+
+    """
+
+
+"""
+txt = "apple#banana#cherry#orange"
+
+# setting the maxsplit parameter to 1, will return a list with 2 elements!
+x = txt.split("#", 2)
+
+print(x)
+"""
+"""
+
     # not pass
     def reorderLogFiles(self, G):
         A = []
@@ -25,6 +70,7 @@ class Solution:
                 B.append(g)
         final = [" ".join(i) for i in sorted(A, key = lambda x: x[1:] + [x[0]]) + B]
         return sorted(final)
+"""
 """
 937. Reorder Data in Log Files
 Easy

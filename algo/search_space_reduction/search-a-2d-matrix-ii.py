9dda399e3cc59cd2041900017be6a13571e7a8ac
GreenMarch <lucuiwang@gmail.com>
2020-02-04 20:52:46 -0800

    2d array

---
+++ algo/search_space_reduction/search-a-2d-matrix-ii.py
@@ -0,0 +1,55 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        height, width = len(matrix), len(matrix[0])
+
+        row = height - 1
+        col = 0
+
+        while col < width and row >= 0:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:
+                return True
+
+        return False
+
+"""
+240. Search a 2D Matrix II
+Medium
+
+2331
+
+65
+
+Add to List
+
+Share
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
+5
+"""

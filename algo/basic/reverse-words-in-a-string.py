28afdee2c092595c5c42f78e62a76773437a1079
GreenMarch <GreenMarch@users.noreply.github.com>
2020-02-11 22:50:19 -0800

    Update reverse-words-in-a-string.py

--- algo/basic/reverse-words-in-a-string.py
+++ algo/basic/reverse-words-in-a-string.py
@@ -1,3 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(x for x in s.split()[::-1])
+        # return " ".join(reversed(s.split())
+        # return " ".join((s.split()[::-1])

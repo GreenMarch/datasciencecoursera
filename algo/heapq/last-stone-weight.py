42c706b711219d044f2e9c08536e1a1dc666f0f4
GreenMarch <lucuiwang@gmail.com>
2020-02-19 20:14:47 -0800

    heap

--- algo/heapq/last-stone-weight.py
+++ algo/heapq/last-stone-weight.py
@@ -1,27 +1,27 @@
-from heapq import heappush, heappop, heapify
+from heapq import heappop, heappush, heapify
+
 class Solution:
-    def lastStoneWeight(self, stones):
+    def lastStoneWeight(self, stones: List[int]) -> int:
         if len(stones) == 1:
-            print("len is one")
-            print(str(stones))
-            return stones[0]
+            return stones
         else:
-            for idx, val in enumerate(stones):
-                stones[idx] = val*-1
+            for i, v in enumerate(stones):
+                stones[i] = v * -1
             heapify(stones)
             res = 0
             while len(stones) > 1:
-                stone1 = heappop(stones)
-                stone2 = heappop(stones)
-                if stone1 == stone2:
+                s1 = heappop(stones)
+                s2 = heappop(stones)
+                if s1 == s2:
                     continue
                 else:
-                    res = stone1 - stone2
+                    res = s1 - s2 if s1 < s2 else s2 - s1
                     heappush(stones, res)
             if stones:
-                return stones[0]*-1
+                return stones[0] * -1
             else:
                 return 0
+
 
 inputdata = [2,7,4,1,8,1]
 print(Solution().lastStoneWeight(inputdata))

1cfa7f3af5d3e285fc26c280b96ef373820377c5
GreenMarch <lucuiwang@gmail.com>
2020-02-19 22:36:45 -0800

    heap

---
+++ algo/heap/top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
+
+        # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     count = Counter(nums)
+    #     return list(zip(*count.most_common(k)))[0]

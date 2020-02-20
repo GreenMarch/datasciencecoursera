14155a3d929cdb3e149063732ce6fb12d84c71c2
GreenMarch <lucuiwang@gmail.com>
2020-02-19 23:15:26 -0800

    heap

--- algo/heap/kth-largest-element-in-an-array.py
+++ algo/heap/kth-largest-element-in-an-array.py
@@ -13,3 +13,26 @@ class Solution:
 data = [3,2,1,5,6,4]
 k = 2
 print(Solution().findKthLargest(data, k))
+
+"""
+215. Kth Largest Element in an Array
+Medium
+
+2941
+
+208
+
+Add to List
+
+Share
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+"""

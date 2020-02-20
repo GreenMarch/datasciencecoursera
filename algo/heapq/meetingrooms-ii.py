6c7bee43f661b60ff60086153b2cf4e04c74d838
GreenMarch <lucuiwang@gmail.com>
2020-02-19 20:55:00 -0800

    heap

--- algo/heapq/meetingrooms-ii.py
+++ algo/heapq/meetingrooms-ii.py
@@ -1,9 +1,13 @@
+from heapq import heappush, heapreplace
 class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+    def minMeetingRooms(self, intervals):
         h = []
         for i in sorted(intervals):
             if h == [] or h[0] > i[0]:
-                heapq.heappush(h, i[1])
+                heappush(h, i[1])
             else:
-                heapq.heapreplace(h, i[1])
+                heapreplace(h, i[1])
         return len(h)
+
+inputdata = [[0,30],[5,10],[15,20]]
+print(Solution().minMeetingRooms(inputdata))

8a35f3f5d38da8ee0979b8081b84d380bcd3d9d4
GreenMarch <lucuiwang@gmail.com>
2020-04-15 23:40:20 -0700

    segment tree

---
+++ algo/segment_tree/range-sum-query-2d-immutable.py
@@ -0,0 +1,54 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        if matrix is None or not matrix:
+            return
+        n, m = len(matrix), len(matrix[0])
+        self.sums = [ [0 for j in range(m + 1)] for i in range(n + 1) ]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                self.sums[i][j] = matrix[i - 1][j - 1] + self.sums[i][j - 1] + self.sums[i - 1][j] - self.sums[i - 1][j - 1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1, col1, row2, col2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        return self.sums[row2][col2] - self.sums[row2][col1 - 1] - self.sums[row1 - 1][col2] + self.sums[row1 - 1][col1 - 1]
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+# The idea is simple, just precompute sums for all matrices with (0, 0) as top left corner and (i, j) as bottom right corner. There are O(n^2) of these matrices, so we store them in a 2D table. In order to make code simpler, I add an extra column and row, filled with 0.
+
+"""
+304. Range Sum Query 2D - Immutable
+Medium
+
+809
+
+164
+
+Add to List
+
+Share
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+"""

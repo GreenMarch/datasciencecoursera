4cc387205151af857c3a739dcc83513c6011a01a
GreenMarch <lucuiwang@gmail.com>
2020-04-15 23:22:56 -0700

    segment tree

--- algo/heap/merge-k-sorted-lists.py
+++ algo/heap/merge-k-sorted-lists.py
@@ -1,8 +1,21 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def Creatlist(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return ListNode(1)  # 只有一个节点
+    else:
+        root = ListNode(1)
+        tmp = root
+        for i in range(2, n + 1):  # 一个一个的增加节点
+            tmp.next = ListNode(i)
+            tmp = tmp.next
+    return root  # 返回根节点
 
 class Solution(object):
     def mergeKLists(self, lists):
@@ -10,7 +23,6 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-
         h = [(l.val, idx) for idx, l in enumerate(lists) if l]
         heapq.heapify(h)
         head = cur = ListNode(None)
@@ -49,15 +61,19 @@ Output: 1->1->2->3->4->4->5->6
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 from heapq import heappop, heappush
 
 
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    def mergeKLists(self, lists):
+        """
+        :param lists: List[ListNode]
+        :return: ListNode
+        """
 
         values, head, pointer = [], None, None
 
@@ -75,3 +91,26 @@ class Solution:
                 pointer = pointer.next
 
         return head
+
+def main():
+    print("Create a linklist")
+    head = Creatlist(7)
+    printlist(head)
+    print
+    print
+    "___________________________"
+
+    n1 = input("Enter the index to insert")
+    n1 = int(n1)
+    insert(head, n1)
+    printlist(head)
+    print()
+    print("___________________________")
+
+    n2 = input("Enter the index to delete")
+    n2 = int(n2)
+    dellist(head, n2)
+    printlist(head)
+
+
+if __name__ == '__main__':  main()  # 主函数调用

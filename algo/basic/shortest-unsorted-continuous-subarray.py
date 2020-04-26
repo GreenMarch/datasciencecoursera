32f27633066f1fb972e15ce1dfa451ffec2ad11c
GreenMarch <lucuiwang@gmail.com>
2020-04-26 12:13:00 -0700

    array

--- algo/basic/shortest-unsorted-continuous-subarray.py
+++ algo/basic/shortest-unsorted-continuous-subarray.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def findUnsortedSubarray(self, nums):
         sortedNums = sorted(nums)
--- algo/heap/top-k-frequent-elements.py
+++ algo/heap/top-k-frequent-elements.py
@@ -3,10 +3,18 @@ import heapq
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums, k):
+        """
+        :param nums: List[int]
+        :param k: int
+        :return: List[int]
+        """
         count = Counter(nums)
         return heapq.nlargest(k, count.keys(), key=count.get)
 
         # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
     #     count = Counter(nums)
     #     return list(zip(*count.most_common(k)))[0]
+
+inputlist = [1,2,2,3,3,3]
+print(Solution().topKFrequent(inputlist, 2))

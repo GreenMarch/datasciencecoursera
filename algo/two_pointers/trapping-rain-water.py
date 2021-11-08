86d97d4dccf628e95c4bb9cdce9dab0a1e9fdffd
GreenMarch <GreenMarch@users.noreply.github.com>
2021-11-08 00:27:01 -0800

    Update trapping-rain-water.py

--- algo/two_pointers/trapping-rain-water.py
+++ algo/two_pointers/trapping-rain-water.py
@@ -18,6 +18,27 @@ class Solution(object):
                 volume += r_max - height[right]
                 right -= 1
         return volume
+    
+#         """
+#         https://www.youtube.com/watch?v=ZI2z5pq0TqA
+#         """
+#         if not height: return 0
+        
+#         l, r = 0, len(height) - 1
+#         leftMax, rightMax = height[l], height[r]
+#         res = 0
+        
+#         while l < r:
+#             if leftMax < rightMax:
+#                 l += 1
+#                 leftMax = max(leftMax, height[l])
+#                 res += leftMax - height[l]
+#             else:
+#                 r -= 1
+#                 rightMax = max(rightMax, height[r])
+#                 res += rightMax - height[r]
+#         return res
+
 """
 42. Trapping Rain Water
 Hard
